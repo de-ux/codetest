a58d4db59e538811e2952a9a6089fae4da283b51
Brett <bfrable@redventures.com>
2018-01-13 08:33:36 -0500

    updated signout

--- todoapp/controllers/signout.js
+++ todoapp/controllers/signout.js
@@ -1,11 +1,5 @@
 (function() {
   todoApp.controller('SignoutCtrl', ['$state', function ($state) {
-
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        firebase.auth().signOut()
-        $state.go('home', {}, { reload: 'home' });
-      }
-    });
-  }]);
+    firebase.auth().signOut()
+    $state.go('home', {}, { reload: 'home' });  }]);
 }());
