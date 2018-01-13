435a2f28765b8c0310cfdd422e9aa6643f882e3b
Brett <bfrable@redventures.com>
2018-01-13 08:56:43 -0500

    removed phone sign in option

--- todoapp/controllers/home.js
+++ todoapp/controllers/home.js
@@ -6,7 +6,6 @@
       'credentialHelper': firebaseui.auth.CredentialHelper.NONE,
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
       ]
     };
 
