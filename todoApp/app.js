4c58e294584dad7b6cc85bcc2601fc859ea093fa
Louise Clark <lclark070607@gmail.com>
2019-01-06 17:32:36 -0500

    revised logic for add button

--- todoApp/app.js
+++ todoApp/app.js
@@ -1,6 +1,12 @@
 //event handler object for the input button
 let eventHandlers = {
     ableSubmitBtn: function () {
-        document.getElementById("submit-btn").disabled = false;
+        let todoInput = document.getElementById("todo-input");
+
+        if (todoInput.value === "") {
+            document.getElementById("submit-btn").disabled = true;
+        } else {
+            document.getElementById("submit-btn").disabled = false;
+        };
     }
-}
+};
